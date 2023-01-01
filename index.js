97bdf7b1934687229444bfef05fc8aa42a876c42
Chayan <chayansarkar2001@gmail.com>
2023-01-01 17:48:04 +0530

    commit1

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express from "express"
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("welcome to render deploy")
+    res.status(200)
+    res.json({message:"welcome to render deploy"})
+})
+app.get('*',(req,res)=>{
+    res.status(200).json({message:"Page can't find"})
 })
 
 app.listen(5000,()=>console.log("listning in 5000"))
